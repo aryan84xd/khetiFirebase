d6f9c587dbab82b2ed397ad9abfe795466a82a31
ARYANHULE\aryan <aryanhule84@gmail.com>
2025-04-20 21:39:52 +0530

    key issue fixed

--- khetisahayak/src/utils/firebase.js
+++ khetisahayak/src/utils/firebase.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCXX2P75QFsaP-NDm9JWvuSFLPd1XhSj7Y",
-  authDomain: "kheti2-380ec.firebaseapp.com",
-  projectId: "kheti2-380ec",
-  storageBucket: "kheti2-380ec.firebasestorage.app",
-  messagingSenderId: "3961871095",
-  appId: "1:3961871095:web:e4948b443d0fa647af11f2",
-  measurementId: "G-FZYLL5Z3TB",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
